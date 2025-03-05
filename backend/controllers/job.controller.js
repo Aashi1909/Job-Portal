6d769ed86cbe9f9c4562f448fa905fcf27122a3d
Aashi <aashiaggarwal28552@gmail.com>
2025-03-05 23:46:18 +0530

    postjob api created

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,27 @@
+import {Job} from "../models/job.model.js";
+
+export const postJob = async (req, res) => {
+    try{
+        const {title, description,requiremnts, location, salary, jobType, position, experience, companyId} = req.body;
+        const userId = req.id;
+        if(!title || !description || !requiremnts || !location || !salary || !jobType || !position || !experience || !companyId ){
+            return res.status(400).json({message: "All fields are required", success:false});
+        }
+        const job = await Job.create({title, 
+            description,
+            requiremnts: requiremnts.split(","),
+            location, 
+            salary,
+            jobType,
+            position, 
+            experienceLevel: experience, 
+            company : companyId,
+            created_by : userId
+        })
+        return res.status(201).json({message: "Job Posted successfully", job, success:true});
+
+    }catch(error){
+
+    }
+}
+
